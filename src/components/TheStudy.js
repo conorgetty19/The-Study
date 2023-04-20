7ef6e1c05b65d8424c1f6a089de5ed6749c0a1c5
Conor Getty <conorgetty@gmail.com>
2023-04-20 15:19:19 -0500

    finished feature. registration page renders properly

--- src/components/TheStudy.js
+++ src/components/TheStudy.js
@@ -1,22 +1,24 @@
 import {Route, Routes} from "react-router-dom"
 import { Authorized } from "./views/Authorized";
 import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 import './TheStudy.css';
 import { ApplicationViews } from "./views/ApplicationViews";
 
+
+//add navbar later
 export const TheStudy = () => {
   return <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<h2>register</h2>}/>
+      <Route path="/register" element={<Register />}/>
 
       <Route path="*" element={
         <Authorized>
           <>
-          
+
           <ApplicationViews />
           </>
         </Authorized>
       } />
   </Routes>
 }
-//test
