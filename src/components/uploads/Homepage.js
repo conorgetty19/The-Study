d4d90f842f9cbdb2caeebfedf3c09250231b59e7
Conor Getty <conorgetty@gmail.com>
2023-05-08 14:42:20 -0500

    all uploads render on homepage

--- src/components/uploads/Homepage.js
+++ src/components/uploads/Homepage.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
-import { useNavigate, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { Resource } from "./Resource"
 
 export const Homepage = () => {
-    const navigate = useNavigate()
     const [categories, setCategories] = useState([])
+    const [uploads, setUploads] = useState([])
 
     useEffect(
         () => {
@@ -16,14 +17,47 @@ export const Homepage = () => {
         []
     )
 
+    const getAllResources = () => {
+        fetch(`http://localhost:8088/resources`)
+            .then(res => res.json())
+            .then((resources) => {
+                setUploads(resources)
+            })
+    }
+
+    useEffect(
+        () => {
+            getAllResources()
+        },
+        []
+    )
+
     return (
-        <main className="general-font page-content">
+        <main className="general-font page-content allCategories">
             <h1>Research Categories</h1>
-            <div className=" homepage-category d-flex flex-column justify-content-between">
-            {categories.map((category) => {
-                return <div key={category.id}><Link className="custom-text-green" to={`/category/${category.id}`} id={category.id}>Category {category.id}: {category.type}</Link><br/></div>
-            })}
-            </div>
+            <article className=" homepage-category d-flex flex-column justify-content-between">
+                {categories.map((category) => {
+                    return <div key={category.id}><Link className="custom-text-green" to={`/category/${category.id}`} id={category.id}>Category {category.id}: {category.type}</Link><br /></div>
+                })}
+            </article>
+            <article className="allUploads">
+                <h2 className="h1 allUploads-title">All Uploads</h2>
+                <section className=" d-flex justify-content-between flex-wrap">
+                    {
+                        uploads.map((upload) => <Resource
+                            key={`upload-${upload.id}`}
+                            id={upload.id}
+                            link={upload.url}
+                            creator={upload.creatorId}
+                            title={upload.title}
+                            img={upload.image}
+                            format={upload?.format?.type}
+                            description={upload.description}
+                            getAllResources={getAllResources}
+                        />)
+                    }
+                </section>
+            </article>
         </main>
     )
 }
